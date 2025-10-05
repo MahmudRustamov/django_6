760c48df59eee8870a9664d8629e4269b4ff8078
Mahmudbek <rustamov.mahmud.05@gmail.com>
2025-10-05 18:04:30 +0500

    wish

--- apps/wishlist/urls.py
+++ apps/wishlist/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from apps.wishlist.views import wishlist_list, wishlist_add, wishlist_remove
+from . import views
 
 app_name = 'wishlist'
 
-
 urlpatterns = [
-    path('', wishlist_list, name='list'),
-    path('add/<int:product_id>/', wishlist_add, name='wishlist_add'),
-    path('remove/<int:product_id>/', wishlist_remove, name='wishlist_remove'),
+    path('', views.wishlist_list, name='list'),
+    path('add/<int:product_id>/', views.wishlist_add, name='wishlist_add'),
+    path('remove/<int:product_id>/', views.wishlist_remove, name='wishlist_remove'),
 ]
--- apps/wishlist/views.py
+++ apps/wishlist/views.py
@@ -1,26 +1,31 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-
 from apps.products.models import ProductModel
 from apps.wishlist.models import WishlistModel
 
 
 @login_required
 def wishlist_add(request, product_id):
+    """Mahsulotni foydalanuvchi wishlistiga qo‘shadi"""
     product = get_object_or_404(ProductModel, id=product_id)
     WishlistModel.objects.get_or_create(user=request.user, product=product)
-    return redirect(request.META.get('HTTP_REFERER', 'products:home'))
+    return redirect(request.META.get('HTTP_REFERER', 'wishlist:list'))
 
 
 @login_required
 def wishlist_remove(request, product_id):
+    """Wishlistdan mahsulotni o‘chiradi"""
     product = get_object_or_404(ProductModel, id=product_id)
     WishlistModel.objects.filter(user=request.user, product=product).delete()
-    return redirect(request.META.get('HTTP_REFERER', 'products:home'))
+    return redirect(request.META.get('HTTP_REFERER', 'wishlist:list'))
 
 
 @login_required
 def wishlist_list(request):
-    items = WishlistModel.objects.filter(user=request.user)
-    return render(request, 'products/user-wishlist.html', {'items': items})
+    """Foydalanuvchining wishlistini ko‘rsatadi"""
+    items = WishlistModel.objects.filter(user=request.user).select_related('product')
+    return render(request, 'auth/user-wishlist.html', {'items': items})
 
+
+def in_wishlist(self, product_id):
+    return str(product_id) in self.basket.keys()
